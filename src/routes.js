96d37b67368133c300b310cfe2ff4ed8d60bf6cb
Jefferson <dev.jefferson.m@gmail.com>
2020-05-05 19:13:06 -0300

    chore: updating routes
    chore: updating routes

--- src/routes.js
+++ src/routes.js
@@ -10,15 +10,21 @@ const routes = new Router();
 routes.post('/register', UserControll.create);
 routes.post('/session', SessionControll.create);
 
+/* Verify Token */
+
 routes.use(authVerify);
 
 /* User */
+
+routes.get('/users', UserControll.index);
+routes.put('/user', UserControll.update);
+routes.delete('/user', UserControll.delete);
+
+/* Task */
 
 routes.post('/task/new', TaskControll.create);
 routes.get('/tasks', TaskControll.index);
 routes.put('/task/:id', TaskControll.update);
 routes.delete('/task/:id', TaskControll.delete);
-
-/* Task */
 
 export default routes;
